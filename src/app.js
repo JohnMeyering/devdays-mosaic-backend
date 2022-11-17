5c709726aa26de128d111bceb80e251557b10c4f
Tran Ngo <trxnngo@gmail.com>
2022-11-17 13:48:50 -0500

    Run initial Prisma migrate command to create SQLite database

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import { PrismaClient } from "@prisma/client";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const prisma = new PrismaClient();
+
+app.get("/admin/images", async (req, res) => {
+  const images = await prisma.post.findMany({
+    where: { published: true },
+    include: { author: true },
+  });
+  res.json(posts);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
